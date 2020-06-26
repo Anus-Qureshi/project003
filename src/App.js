6df733e001c9ee8631a2e2139cc238a07c4f42cf
Anus-Qureshi <66444837+Anus-Qureshi@users.noreply.github.com>
2020-06-26 15:58:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. </p>
-      <p> Maryam Abbasi Sahiba , Anus really loves you   </p> 
-     <p> or Ami se bh bt krni he.</p>
+     <p>Maryam ! I want to tell you something. </p>
+ <p> Listen , I have told Ami about us because I want to give a "Naam" to our Be-naam relation </p>
+ <p> Is trh se hath pe hath rkh k Bethe rhengy to Kuch nh hoga  </p>
+ <p> Is bary me socho  Or last me yhi Kahnga  </p>
+ <p> Maryam Abbasi Sahiba , Anus Qureshi really loves you   </p> 
+     
         <a
           className="App-link"
           href="https://m.facebook.com/profile.php?id=100052436600530&pn_ref=ec_friends_card&ref=bookmarks"
@@ -18,6 +22,14 @@ function App() {
           rel="noopener noreferrer"
         >
          Your fb id link click here (;
+        </a>
+<a
+          className="App-link"
+          href="https://m.facebook.com/anas.qureshi.7902?ref=bookmarks"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         My fb id link 
         </a>
       </header>
     </div>
