77126a52ea5323defc1c9ec43042f0c1dd535b89
Anus-Qureshi <66444837+Anus-Qureshi@users.noreply.github.com>
2020-06-26 15:24:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. </p>
       <p> Maryam Abbasi Sahiba , Anus really loves you   </p> 
-    
+     <p> or Ami se bh bt krni he.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://m.facebook.com/profile.php?id=100052436600530&pn_ref=ec_friends_card&ref=bookmarks"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Your fb id link click here (;
         </a>
       </header>
     </div>
